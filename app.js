7a0634cee31b85de88e846deb5657cc0519a9af3
Dominik Rohal <rohal.dominik.123@gmail.com>
2024-02-26 12:27:46 +0200

    Exercise 11.12 simulated error

--- app.js
+++ app.js
@@ -8,10 +8,8 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/health', (req, res) => {
-  const a = 1
-  if (a === 1){
-    throw('error...  ')
-  }
+  // eslint-disable-next-line no-constant-condition
+  if (true) throw('error...  ')
   res.send('ok')
 })
 
